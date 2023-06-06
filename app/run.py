8043de955709da513bba3a0e41d01fc2181de249
hedeya1980 <hedeya1980@gmail.com>
2023-06-06 23:33:36 +0300

    Prepare for Heroku deployment 5

--- app/run.py
+++ app/run.py
@@ -13,7 +13,7 @@ from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
 #from sklearn.externals import joblib
 import joblib
-from sqlalchemy import create_engine
+#from sqlalchemy import create_engine
 
 from classDefs import ItemSelector, StartingVerbExtractor, MessageLengthExtractor
 
